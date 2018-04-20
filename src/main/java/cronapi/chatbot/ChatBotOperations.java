e9040392e1ac3d2028f5066aa779d2d51f51f3b3
Michel Moreira <michel@michelmoreira.eti.br>
2018-04-20 10:42:03 -0300

    ChatBotOperations

---
+++ src/main/java/cronapi/chatbot/ChatBotOperations.java
@@ -0,0 +1,62 @@
+package cronapi.chatbot;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+import cronapi.CronapiMetaData;
+import cronapi.telegram.bots.TelegramBotOperations;
+import cronapi.telegram.bots.methods.SendMessage;
+import cronapi.telegram.bots.models.InlineKeyboardButton;
+import cronapi.telegram.bots.models.InlineKeyboardMarkup;
+import cronapi.telegram.bots.models.ReplyMarkup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@CronapiMetaData
+public class ChatBotOperations {
+
+    @CronapiMetaData
+    public static void sendWatsonMessageToTelegram(MessageResponse watsonResponse, String telegramChatId,
+                                                   String telegramToken) throws IOException {
+        SendMessage sendMessage = null;
+
+        for (String outputText: watsonResponse.getOutput().getText())
+        {
+            if (sendMessage != null)
+                TelegramBotOperations.sendMessage(sendMessage);
+
+            sendMessage = new SendMessage();
+            sendMessage.setText(outputText);
+            sendMessage.setToken(telegramToken);
+            sendMessage.setChatId(telegramChatId);
+
+        }
+
+        if (sendMessage != null) {
+            if (watsonResponse.getContext().containsKey("quick_reply")) {
+                List<InlineKeyboardButton> inlineKeyboardRow = new ArrayList<>();
+
+                List<Map<String, String>> quickReplyList =
+                        (List<Map<String, String>>) watsonResponse.getContext().get("quick_reply");
+                for (Map<String, String> quickReply : quickReplyList) {
+                    InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+                    inlineKeyboardButton.setText(quickReply.get("text"));
+                    inlineKeyboardButton.setCallbackData(quickReply.get("data"));
+                    inlineKeyboardRow.add(inlineKeyboardButton);
+                }
+
+                InlineKeyboardMarkup replyMarkup = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> inlineKeyboard = new ArrayList<>();
+                inlineKeyboard.add(inlineKeyboardRow);
+                replyMarkup.setInlineKeyboard(inlineKeyboard);
+                sendMessage.setReplyMarkup(replyMarkup);
+            }
+
+            TelegramBotOperations.sendMessage(sendMessage);
+        }
+        //new JsonParser().parse()
+    }
+}
