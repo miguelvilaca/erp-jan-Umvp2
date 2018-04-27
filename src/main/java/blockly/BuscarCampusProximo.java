b52c3b5eca3f2c42f27d8cfdab733351b956b054
Humberto Favoretto <humberto.favoretto@techne.com.br>
2018-04-27 19:36:03 +0000

    Commit de pesquisa por cep

--- src/main/java/blockly/BuscarCampusProximo.java
+++ src/main/java/blockly/BuscarCampusProximo.java
@@ -21,8 +21,10 @@ public class BuscarCampusProximo {
 		return new Callable<Var>() {
 
 			private Var watsonContext = Var.VAR_NULL;
-			private Var codigoCurso = Var.VAR_NULL;
 			private Var cep = Var.VAR_NULL;
+			private Var codigoCursoBruto = Var.VAR_NULL;
+			private Var posicaoDoPonto = Var.VAR_NULL;
+			private Var codigoCurso = Var.VAR_NULL;
 			private Var jsonPolosProximos = Var.VAR_NULL;
 			private Var listaPolosProximos = Var.VAR_NULL;
 			private Var campus = Var.VAR_NULL;
@@ -30,9 +32,13 @@ public class BuscarCampusProximo {
 
 			public Var call() throws Exception {
 				watsonContext = cronapi.object.Operations.getObjectField(watsonMessage, Var.valueOf("context"));
-				codigoCurso = cronapi.json.Operations.getJsonOrMapField(watsonContext, Var.valueOf("$.idCurso"));
 				cep = cronapi.object.Operations.getObjectField(watsonMessage,
 						Var.valueOf("$.entities[?(@.entity==\'sys-number\')].value"));
+				codigoCursoBruto = cronapi.map.Operations.getJsonOrMapField(watsonContext, Var.valueOf("$.idCurso"));
+				posicaoDoPonto = Var.valueOf(
+						codigoCursoBruto.getObjectAsString().indexOf(Var.valueOf(".").getObjectAsString()) + 1);
+				codigoCurso = cronapi.text.Operations.getLettersFromStartToFromStart(codigoCursoBruto, Var.valueOf(0),
+						(cronapi.math.Operations.subtract(posicaoDoPonto, Var.valueOf(1))));
 				jsonPolosProximos = cronapi.util.Operations.getURLFromOthers(Var.valueOf("GET"),
 						Var.valueOf("application/json"),
 						Var.valueOf(Var.valueOf("https://www.unip.br/ead/api/servicos/cursos/").toString()
