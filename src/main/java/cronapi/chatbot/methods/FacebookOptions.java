0c81e2b72d7c180861b80960ba478b5610a884fb
Michel Moreira <michel@michelmoreira.eti.br>
2018-04-27 16:11:12 -0300

    Definição de Contexto no Quick Reply

---
+++ src/main/java/cronapi/chatbot/methods/FacebookOptions.java
@@ -0,0 +1,31 @@
+package cronapi.chatbot.methods;
+
+public class FacebookOptions {
+    private String pageAccessToken;
+    private String appSecret;
+    private String verifyToken;
+
+    public String getPageAccessToken() {
+        return pageAccessToken;
+    }
+
+    public void setPageAccessToken(String pageAccessToken) {
+        this.pageAccessToken = pageAccessToken;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public String getVerifyToken() {
+        return verifyToken;
+    }
+
+    public void setVerifyToken(String verifyToken) {
+        this.verifyToken = verifyToken;
+    }
+}
